fccbd4ec3357282c34820212ac67356c51b899b8
Daniel Bailo <danbailoufms@gmail.com>
2019-06-01 00:24:53 -0400

    aprendendo numpy

---
+++ NumPy/array/foo.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+my_list = [1, 2, 3]
+
+print(my_list)
+
+np_array = np.array(my_list)
+
+print(np_array) #vetor
+
+my_matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+    ]
+
+print(my_matrix)
+
+np_matrix = np.array(my_matrix) 
+
+print(np_matrix) #matriz de duas dimensoes
